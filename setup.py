e6d1f68a79d08991d513935f5fc2886d84b4c11a
vrde <agranzot@gmail.com>
2016-01-11 16:17:46 +0100

    Update setup.py with stuff for pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
-#!/usr/bin/env python
-
 from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='logstats',
-    version='0.1',
+    version='0.1.0',
+
     description='A module to collect and display stats for long running processes',
+    long_description=long_description,
+
+    url='https://github.com/vrde/logstats',
+
     author='Alberto Granzotto',
     author_email='agranzot@gmail.com',
-    packages=['logstats'],
-    test_suite='logstats.test'
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+    ],
+
+    packages=['logstats']
 )
