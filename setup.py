e1e8212b3c7810131d212b508683406f0d93d3af
vrde <agranzot@gmail.com>
2016-01-18 14:30:43 +0100

    Sorry long_description, you are so annoying

--- setup.py
+++ setup.py
@@ -1,28 +1,12 @@
 from setuptools import setup
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-try:
-    from pypandoc import convert
-    read_md = lambda f: convert(f, 'rst')
-except ImportError:
-    import warnings
-    warnings.warn('warning: pypandoc module not found, could not convert Markdown to RST')
-    read_md = lambda f: open(f, 'r').read()
-
 
 
 setup(
     name='logstats',
-    version='0.2.0',
+    version='0.2.1',
 
     description='A module to collect and display stats for long running processes',
-    long_description=read_md('README.md'),
+    long_description='For a long description please visit https://github.com/vrde/logstats',
 
     url='https://github.com/vrde/logstats',
 
@@ -36,13 +20,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.2',
-        # 'Programming Language :: Python :: 3.3',
-        # 'Programming Language :: Python :: 3.4',
-        # 'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3'
     ],
 
     packages=['logstats']
